7c9d0921e87397d9176f3e82f71e4d63f8c7ff32
jpsfs <jpsfs@hotmail.com>
2012-12-03 15:28:10 +0000

    Testing

---
+++ src/up/pt/mads/hackthon2/main/Main.java
@@ -0,0 +1,9 @@
+package up.pt.mads.hackthon2.main;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+	      System.out.println("Hello World");
+	}
+}
---
+++ src/up/pt/mads/hackthon2/unittests/Main.java
@@ -0,0 +1,14 @@
+package up.pt.mads.hackthon2.unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Main {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
